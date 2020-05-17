284aeffeae547477659d92d69b50dd3937de0cde
meuprojetoversionado448 <einz@mylittleali.tk>
2020-05-17 12:50:17 -0300

    Auth

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::resource("admin/plans", "Admin\PlanController@index")->name('plans.index');
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
