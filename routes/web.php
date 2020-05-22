3dc090a3f92d5a91ff3b9560b20df07be8da2c81
meuprojetoversionado448 <einz@mylittleali.tk>
2020-05-22 15:12:39 -0300

    group routes

--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->group(function () {
+
+        Route::get("plans/create", "PlanController@create")->name('plans.create');
+        Route::put("plans/{url}/update", "PlanController@update")->name('plans.update');
+        Route::get("plans/{url}/edit", "PlanController@edit")->name('plans.edit');
+        Route::any("plans/search", "PlanController@search")->name('plans.search');
+        Route::delete("plans/{url}", "PlanController@destroy")->name('plans.destroy');
+        Route::get("plans/{url}", "PlanController@show")->name('plans.show');
+        Route::get("plans", "PlanController@index")->name('plans.index');
+        Route::post("plans", "PlanController@store")->name('plans.store');
+
+        Route::get("/", "Admin\PlanController@index")->name('admin.index');
+    });
+
 //Route::resource("admin/plans", "Admin\PlanController@index")->name('plans.index');
-Route::get("admin/plans/create", "Admin\PlanController@create")->name('plans.create');
-Route::put("admin/plans/{url}/update", "Admin\PlanController@update")->name('plans.update');
-Route::get("admin/plans/{url}/edit", "Admin\PlanController@edit")->name('plans.edit');
-Route::any("admin/plans/search", "Admin\PlanController@search")->name('plans.search');
-Route::delete("admin/plans/{url}", "Admin\PlanController@destroy")->name('plans.destroy');
-Route::get("admin/plans/{url}", "Admin\PlanController@show")->name('plans.show');
-Route::get("admin/plans", "Admin\PlanController@index")->name('plans.index');
-Route::post("admin/plans", "Admin\PlanController@store")->name('plans.store');
 
-
-
-Route::get("admin", "Admin\PlanController@index")->name('admin.index');
 
 Route::get('/', function () {
     return view('welcome');
@@ -33,4 +38,4 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
