a86d90bff7c5e9aef70057fe066a7acd0e8ed733
meuprojetoversionado448 <einz@mylittleali.tk>
2020-06-09 11:59:03 -0300

    debugBar

--- app/Http/Controllers/Site/SiteController.php
+++ app/Http/Controllers/Site/SiteController.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Controllers\Site;
 
-use App\Http\Controllers\Controller;
+use App\Models\Plan;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class SiteController extends Controller
 {
     public function index(Request $request)
     {
 
+        $plans = Plan::with('details')->get();
 
-
-        return view("site.pages.home.index");
+        return view("site.pages.home.index", compact('plans'));
     }
 }
--- resources/views/site/pages/home/index.blade.php
+++ resources/views/site/pages/home/index.blade.php
@@ -7,80 +7,33 @@
     <h1 class="title-plan">Escolha o plano</h1>
 </div>
 <div class="row">
-    <div class="col-md-4 col-sm-6">
-        <div class="pricingTable">
-            <div class="pricing-content">
-                <div class="pricingTable-header">
-                    <h3 class="title">Free</h3>
-                </div>
-                <div class="inner-content">
-                    <div class="price-value">
-                        <span class="currency">R$</span>
-                        <span class="amount">0,00</span>
-                        <span class="duration">Por Mês</span>
+    @foreach ($plans as $plan)    
+        <div class="col-md-4 col-sm-6">
+            <div class="pricingTable">
+                <div class="pricing-content">
+                    <div class="pricingTable-header">
+                        <h3 class="title">{{ $plan->name }}</h3>
                     </div>
-                    <ul>
-                        <li>Categorias</li>
-                        <li>Produtos</li>
-                    </ul>
+                    <div class="inner-content">
+                        <div class="price-value">
+                            <span class="currency">R$</span>
+                            <span class="amount">{{ number_format($plan->price, 2, ',', '.') }}</span>
+                            <span class="duration">Por Mês</span>
+                        </div>
+                        <ul>
+                            @foreach($plan->details as $detail)
+                                <li>{{ $detail->name }}</li>                                
+                            @endforeach
+                        </ul>                        
+                    </div>
                 </div>
-            </div>
-            <div class="pricingTable-signup">
-                <a href="#">Assinar</a>
+                <div class="pricingTable-signup">
+                    <a href="#">Assinar</a>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-md-4 col-sm-6">
-        <div class="pricingTable">
-            <div class="pricing-content">
-                <div class="pricingTable-header">
-                    <h3 class="title">Premium</h3>
-                </div>
-                <div class="inner-content">
-                    <div class="price-value">
-                        <span class="currency">R$</span>
-                        <span class="amount">199,99</span>
-                        <span class="duration">Por Mês</span>
-                    </div>
-                    <ul>
-                        <li>Categorias</li>
-                        <li>Produtos</li>
-                        <li>Mesas</li>
-                        <li>Cardápio</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="pricingTable-signup">
-                <a href="#">Assinar</a>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-4 col-sm-6">
-        <div class="pricingTable">
-            <div class="pricing-content">
-                <div class="pricingTable-header">
-                    <h3 class="title">Business</h3>
-                </div>
-                <div class="inner-content">
-                    <div class="price-value">
-                        <span class="currency">R$</span>
-                        <span class="amount">499,99</span>
-                        <span class="duration">Por Mês</span>
-                    </div>
-                    <ul>
-                        <li>Categorias</li>
-                        <li>Produtos</li>
-                        <li>Mesas</li>
-                        <li>Cardápio</li>
-                        <li>Suporte</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="pricingTable-signup">
-                <a href="#">Assinar</a>
-            </div>
-        </div>
-    </div><!--end-->
+        <!--end-->
+    @endforeach
 </div>
 
 @endsection
