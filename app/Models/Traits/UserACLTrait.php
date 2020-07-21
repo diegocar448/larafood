c5bf24d2c7d575a509f352c6d9f1ab9b3c26910b
Diego Cardoso <diegocar448@hotmail.com>
2020-07-21 14:21:31 -0300

    ACL Auth 2

--- app/Models/Traits/UserACLTrait.php
+++ app/Models/Traits/UserACLTrait.php
@@ -2,13 +2,39 @@
 
 namespace App\Models\Traits;
 
+use App\Models\Tenant;
+
 trait UserACLTrait
 {
 
+    ///////////////////////////////////////////////////////////////////////////////
+    //Retorna todas os cargos do Plano
+    ///////////////////////////////////////////////////////////////////////////////
     public function permissions()
     {
-        $tenant = $this->tenant()->first();
-        $plan = $tenant->plan()->first();
+        $permissionsPlan = $this->permissionsPlan();
+        $permissionsRole = $this->permissionsRole();
+
+        $permissions = [];
+
+        foreach ($permissionsRole as $permissionRole) {
+            if (in_array($permissionRole, $permissionsPlan)) {
+                array_push($permissions, $permissionsPlan);
+            }
+        }
+
+        return $permissions;
+    }
+
+    public function permissionsPlan(): array
+    {
+        /* $tenant = $this->tenant()->first();
+        $plan = $tenant->plan()->first(); */
+        $tenant = Tenant::with('plan.profiles.permissions')->where('id', $this->tenant_id)->first();
+
+        $plan = $tenant->plan;
+
+
 
         $permissions = [];
         foreach ($plan->profiles as $profile) {
@@ -19,6 +45,27 @@ trait UserACLTrait
 
         return $permissions;
     }
+
+
+    ///////////////////////////////////////////////////////////////////////////////
+    //Retorna todas as permissões do cargo do usuario
+    ///////////////////////////////////////////////////////////////////////////////
+    public function permissionsRole(): array
+    {
+        $roles = $this->roles()->with('permissions')->get();
+
+        $permissions = [];
+        foreach ($roles->permissions as $permission) {
+            foreach ($permission->permissions as $permission) {
+                array_push($permissions, $permission->name);
+            }
+        }
+
+        return $permissions;
+    }
+
+
+
     ///////////////////////////////////////////////////////////////////////////////
     //Verifica se ele têm a permissão que é passada como parâmetro
     //Já especificando que o parâmetro será uma string e o retorno será booleano
