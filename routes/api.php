7efdc60c95334c4fa832b5801ef93308923a9f46
Diego Cardoso <diegocar448@hotmail.com>
2020-07-24 10:29:04 -0300

    first commit branch example

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+/* use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route; */
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/* Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+}); */
