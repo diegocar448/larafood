5153b46d973d7582eef0aa8ffd1f9c320894904f
Diego Cardoso <diegocar448@hotmail.com>
2020-08-02 21:52:23 -0300

    API 15

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,38 @@
 <?php
 
+Route::group([
+    "prefix" => "v1",
+    "namespace" => "Api"
+], function () {
 
-Route::get('/tenants/{uuid}', "Api\TenantApiController@show");
-Route::get('/tenants', "Api\TenantApiController@index");
+    Route::get('/tenants/{uuid}', "TenantApiController@show");
+    Route::get('/tenants', "TenantApiController@index");
 
-Route::get('/categories/{url}', "Api\CategoryApiController@show");
-Route::get('/categories', "Api\CategoryApiController@categoriesByTenant");
+    Route::get('/categories/{url}', "CategoryApiController@show");
+    Route::get('/categories', "CategoryApiController@categoriesByTenant");
 
-Route::get('/tables/{identify}', "Api\TableApiController@show");
-Route::get('/tables', "Api\TableApiController@tablesByTenant");
+    Route::get('/tables/{identify}', "TableApiController@show");
+    Route::get('/tables', "TableApiController@tablesByTenant");
 
-Route::get("/products/{flag}", "Api\ProductApiController@show");
-Route::get("/products", "Api\ProductApiController@productsByTenant");
+    Route::get("/products/{flag}", "ProductApiController@show");
+    Route::get("/products", "ProductApiController@productsByTenant");
+});
+
+
+Route::group([
+    "prefix" => "v2",
+    "namespace" => "Api"
+], function () {
+
+    Route::get('/tenants/{uuid}', "TenantApiController@show");
+    Route::get('/tenants', "TenantApiController@index");
+
+    Route::get('/categories/{url}', "CategoryApiController@show");
+    Route::get('/categories', "CategoryApiController@categoriesByTenant");
+
+    Route::get('/tables/{identify}', "TableApiController@show");
+    Route::get('/tables', "TableApiController@tablesByTenant");
+
+    Route::get("/products/{flag}", "ProductApiController@show");
+    Route::get("/products", "ProductApiController@productsByTenant");
+});
